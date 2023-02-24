f6143efa2d750fd825093de188c10fcb67c6a4de
user <you@example.com>
2023-02-24 10:18:31 +0530

    Added web and ports view placeholders

--- ui/src/app/pages/ports-view/ports-view.component.ts
+++ ui/src/app/pages/ports-view/ports-view.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ports-view',
   templateUrl: './ports-view.component.html',
-  styleUrls: ['./ports-view.component.css']
 })
 export class PortsViewComponent {
 
--- ui/src/app/pages/web-view/web-view.component.ts
+++ ui/src/app/pages/web-view/web-view.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'web-view',
   templateUrl: './web-view.component.html',
-  styleUrls: ['./web-view.component.css']
 })
 export class WebViewComponent {
 

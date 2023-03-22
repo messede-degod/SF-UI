f3bc6712e486d45c0e164e8c94144352ace8c2ed
user <you@example.com>
2023-03-22 19:11:52 +0530

    Fix for B0001

--- utils.go
+++ utils.go
@@ -3,9 +3,9 @@ package main
 import "math/rand"
 
 func RandomStr(n int) string {
-	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
-	s := make([]rune, n)
+	s := make([]rune, n-1)
 	for i := range s {
 		s[i] = letters[rand.Intn(len(letters))]
 	}

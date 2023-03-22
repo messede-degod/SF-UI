9e3d610c4308ba2dc84da1cc55acdc8e4df3d600
user <you@example.com>
2023-03-22 19:24:57 +0530

    fix for B0001

--- utils.go
+++ utils.go
@@ -1,13 +1,20 @@
 package main
 
-import "math/rand"
+import (
+	"crypto/rand"
+	"math/big"
+)
 
 func RandomStr(n int) string {
 	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
 	s := make([]rune, n-1)
 	for i := range s {
-		s[i] = letters[rand.Intn(len(letters))]
+		n, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+		if err == nil {
+			s[i] = letters[n.Int64()]
+		}
 	}
+
 	return string(s)
 }

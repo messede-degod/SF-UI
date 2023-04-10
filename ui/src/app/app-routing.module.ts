80328dbf1678c04fbe712846ebd20a3ce8d58e91
user <you@example.com>
2023-04-10 17:08:16 +0530

    Fixed Loading Indication

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { DesktopViewComponent } from './pages/desktop-view/desktop-view.component';
 import { LoginComponent } from './pages/login/login.component';
-import { TerminalViewComponent } from './pages/terminal-view/terminal-view.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full'},

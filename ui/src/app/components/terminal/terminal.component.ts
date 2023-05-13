8d2f3af336e2cfd74ea239b3118319a9691c9773
user <you@example.com>
2023-05-13 13:16:36 +0530

    Refactored Terminal Service

--- ui/src/app/components/terminal/terminal.component.ts
+++ ui/src/app/components/terminal/terminal.component.ts
@@ -4,6 +4,7 @@ import { FitAddon } from 'xterm-addon-fit';
 import { AttachAddonComponent } from '../attach-addon/attach-addon.component';
 import { WebglAddon } from 'xterm-addon-webgl';
 import { Config } from 'src/app/config/config';
+import { TerminalService } from 'src/app/services/terminal.service';
 
 
 @Component({
@@ -13,145 +14,18 @@ import { Config } from 'src/app/config/config';
   encapsulation: ViewEncapsulation.None,
 })
 export class TerminalComponent implements AfterViewInit {
-
-  terminal: Terminal;
-  fitAddon: FitAddon;
-  textEncoder: TextEncoder;
-  textDecoder: TextDecoder;
-  socket!: WebSocket;
-  webglAddon: WebglAddon;
-  termEle!: HTMLElement | null; // Html element within which we render the terminal
-
-  SF_RESIZE: number = 1
-  SF_AUTHENTICATE: number = 4
-  SF_PING: number = 5
-
-  keepAliveInterval!: NodeJS.Timer 
-
   @Input() TermId: number = 0;
-  @Input() AuthToken: string = ""; // To authenticate against ttyd
 
-
-  terminalOptions: ITerminalOptions = {
-    fontSize: 20,
-    cursorBlink: true,
-    fontFamily: 'Consolas,Liberation Mono,Menlo,Courier,monospace',
-    theme: {
-      foreground: '#d2d2d2',
-      background: '#2b2b2b',
-      cursor: '#adadad',
-      black: '#000000',
-      red: '#d81e00',
-      green: '#5ea702',
-      yellow: '#cfae00',
-      blue: '#427ab3',
-      magenta: '#89658e',
-      cyan: '#00a7aa',
-      white: '#dbded8',
-      brightBlack: '#686a66',
-      brightRed: '#f54235',
-      brightGreen: '#99e343',
-      brightYellow: '#fdeb61',
-      brightBlue: '#84b0d8',
-      brightMagenta: '#bc94b7',
-      brightCyan: '#37e6e8',
-      brightWhite: '#f1f1f0',
-    } as ITheme
-  };
-
-  constructor() {
-    if(window.innerWidth<800){
-      this.terminalOptions.fontSize = 14
-    }
-    this.terminal = new Terminal(this.terminalOptions);
-    this.fitAddon = new FitAddon();
-    this.textEncoder = new TextEncoder();
-    this.textDecoder = new TextDecoder();
-    this.webglAddon = new WebglAddon();
+  terminalService: TerminalService
+  constructor(terminalService: TerminalService) {
+    this.terminalService = terminalService
   }
 
-  ngAfterViewInit() {
-    this.termEle = document.getElementById(this.TermId + "")
-    this.fitAddon.activate(this.terminal)
-
-    if (this.termEle != null) {
-      this.terminal.open(this.termEle)
-      this.fitAddon.fit()
-      this.enableWebglRenderer()
-
-      this.socket = new WebSocket(this.getWSURL(), Config.WSServerProtocol);
-
-      // Attach The Sockets I/O to the terminal
-      const attachAddon = new AttachAddonComponent(this.socket, { bidirectional: true });
-      this.terminal.loadAddon(attachAddon);
-
-      this.terminal.writeln("Connecting to SFUI Socket...")
-
-      //Authenticate using Secret
-      this.socket.onopen = () => {
-        this.terminal.clear()
-        
-        const termSecret = {
-          secret: localStorage.getItem('secret')
-        }
-        this.socket?.send(this.SF_AUTHENTICATE + JSON.stringify(termSecret))
-        // Resize Terminal for the first time
-        this.fitAddon.fit();
-      }
-
-      this.socket.onclose = () => {
-        this.terminal.writeln("Terminal Disconnected!")
-      }
-
-      window.onresize = () => {
-        this.fitAddon.fit();
-      };
-
-      // Send Pings at regular interval to prevent socket disconnection
-      this.keepAliveInterval = setInterval(()=>{
-        this.socket.send(String(this.SF_PING))
-      },40*1000) // 40 secs
-
-      this.terminal.onResize(({ cols, rows }) => {
-        const terminal_size = {
-          cols: cols,
-          rows: rows,
-        };
-        this.socket.binaryType = 'blob'
-
-        this.socket.send(this.SF_RESIZE + JSON.stringify(terminal_size));
-      })
-
-    }
+  ngAfterViewInit(): void {
+    this.terminalService.createNewTerminal(this.TermId)
   }
-
-  getWSURL = (): string => {
-    // Determine whether to use ws or wss
-    let wsProto = "ws"
-    if (location.protocol == "https:") {
-      wsProto = "wss"
-    }
-
-    return wsProto + Config.WSServerUrl
-  }
-
-  enableWebglRenderer = () => {
-    try {
-      this.webglAddon.onContextLoss(() => {
-        this.webglAddon?.dispose();
-      });
-      this.terminal.loadAddon(this.webglAddon);
-      console.log('WebGL renderer loaded');
-    } catch (e) {
-      console.log('WebGL renderer could not be loaded', e);
-    }
-  };
 
   ngOnDestroy() {
-    clearInterval(this.keepAliveInterval)
-    this.socket.close()
-    this.termEle?.remove()
-    this.webglAddon.dispose()
-    this.terminal.clear()
+    this.terminalService.removeTerminal(this.TermId)
   }
 }
---
+++ ui/src/app/services/terminal.service.ts
@@ -0,0 +1,209 @@
+import { EventEmitter, Injectable, Output } from '@angular/core';
+import { ITheme, ITerminalOptions, Terminal } from 'xterm';
+import { FitAddon } from 'xterm-addon-fit';
+import { AttachAddonComponent } from '../components/attach-addon/attach-addon.component';
+import { WebglAddon } from 'xterm-addon-webgl';
+import { Config } from 'src/app/config/config';
+
+
+class SfTerminal {
+    termId: number;
+    terminal: Terminal;
+    fitAddon: FitAddon;
+    textEncoder: TextEncoder;
+    textDecoder: TextDecoder;
+    socket!: WebSocket;
+    webglAddon: WebglAddon;
+    termEle!: HTMLElement | null; // Html element within which we render the terminal
+
+    SF_RESIZE: number = 1
+    SF_AUTHENTICATE: number = 4
+    SF_PING: number = 5
+
+    keepAliveInterval!: NodeJS.Timer
+
+    connected: EventEmitter<any> = new EventEmitter();
+    disconnected: EventEmitter<any> = new EventEmitter();
+
+    terminalOptions: ITerminalOptions = {
+        fontSize: 20,
+        cursorBlink: true,
+        fontFamily: 'Consolas,Liberation Mono,Menlo,Courier,monospace',
+        theme: {
+            foreground: '#d2d2d2',
+            background: '#2b2b2b',
+            cursor: '#adadad',
+            black: '#000000',
+            red: '#d81e00',
+            green: '#5ea702',
+            yellow: '#cfae00',
+            blue: '#427ab3',
+            magenta: '#89658e',
+            cyan: '#00a7aa',
+            white: '#dbded8',
+            brightBlack: '#686a66',
+            brightRed: '#f54235',
+            brightGreen: '#99e343',
+            brightYellow: '#fdeb61',
+            brightBlue: '#84b0d8',
+            brightMagenta: '#bc94b7',
+            brightCyan: '#37e6e8',
+            brightWhite: '#f1f1f0',
+        } as ITheme
+    };
+
+    constructor(termId: number) {
+        if (window.innerWidth < 800) {
+            this.terminalOptions.fontSize = 14
+        }
+        this.termId = termId
+        this.terminal = new Terminal(this.terminalOptions);
+        this.fitAddon = new FitAddon();
+        this.textEncoder = new TextEncoder();
+        this.textDecoder = new TextDecoder();
+        this.webglAddon = new WebglAddon();
+    }
+
+
+    create() {
+        this.termEle = document.getElementById(String(this.termId))
+        this.fitAddon.activate(this.terminal)
+
+        if (this.termEle != null) {
+            this.terminal.open(this.termEle)
+            this.fitAddon.fit()
+            this.enableWebglRenderer()
+
+            this.socket = new WebSocket(this.getWSURL(), Config.WSServerProtocol);
+
+            // Attach The Sockets I/O to the terminal
+            const attachAddon = new AttachAddonComponent(this.socket, { bidirectional: true });
+            this.terminal.loadAddon(attachAddon);
+
+            this.terminal.writeln("Connecting to SFUI Socket...")
+
+            //Authenticate using Secret
+            this.socket.onopen = () => {
+                this.terminal.clear()
+
+                const termSecret = {
+                    secret: localStorage.getItem('secret')
+                }
+                this.socket?.send(this.SF_AUTHENTICATE + JSON.stringify(termSecret))
+                // Resize Terminal for the first time
+                this.fitAddon.fit();
+                this.connected.emit(true)
+            }
+
+            window.onresize = () => {
+                this.fitAddon.fit();
+            };
+
+            // Send Pings at regular interval to prevent socket disconnection
+            let keepAliveInterval = setInterval(() => {
+                this.socket.send(String(this.SF_PING))
+            }, 40 * 1000) // 40 secs
+
+            this.socket.onclose = () => {
+                clearInterval(keepAliveInterval)
+                this.disconnected.emit(true)
+                this.terminal.writeln("Terminal Disconnected!")
+            }
+
+            this.terminal.onResize(({ cols, rows }) => {
+                const terminal_size = {
+                    cols: cols,
+                    rows: rows,
+                };
+                this.socket.binaryType = 'blob'
+
+                this.socket.send(this.SF_RESIZE + JSON.stringify(terminal_size));
+            })
+
+        }
+    }
+
+
+    removeTerminal() {
+        this.socket.close()
+        this.termEle?.remove()
+        this.webglAddon.dispose()
+        this.terminal.clear()
+    }
+
+
+    getWSURL = (): string => {
+        // Determine whether to use ws or wss
+        let wsProto = "ws"
+        if (location.protocol == "https:") {
+            wsProto = "wss"
+        }
+
+        return wsProto + Config.WSServerUrl
+    }
+
+    enableWebglRenderer = () => {
+        try {
+            this.webglAddon.onContextLoss(() => {
+                this.webglAddon?.dispose();
+            });
+            this.terminal.loadAddon(this.webglAddon);
+            console.log('WebGL renderer loaded');
+        } catch (e) {
+            console.log('WebGL renderer could not be loaded', e);
+        }
+    };
+
+}
+
+
+@Injectable({
+    providedIn: 'root',
+})
+export class TerminalService {
+
+    terminals: Map<number, SfTerminal> = new Map()
+    activeTerms: number = 0
+    isactive: EventEmitter<any> = new EventEmitter();
+
+    createNewTerminal(termId: number) {
+        this.removeTerminal(termId)
+        let sfTerminal = new SfTerminal(termId)
+        sfTerminal.create()
+        
+        sfTerminal.disconnected.subscribe(() => {
+            this.handleTerminalClose()
+        })
+
+        sfTerminal.connected.subscribe(() => {
+            this.handleTerminalOpen()
+        })
+
+        this.terminals.set(termId, sfTerminal)
+    }
+
+    removeTerminal(termId: number) {
+        let sfTerminal = this.terminals.get(termId)
+        if (sfTerminal != undefined) {
+            sfTerminal.removeTerminal()
+            this.terminals.delete(termId)
+        }
+    }
+
+    private handleTerminalClose() {
+        this.activeTerms -= 1
+        if (this.activeTerms < 1) {
+            console.log("emitting inactive")
+            this.isactive.emit(false)
+        }
+    }
+
+    private handleTerminalOpen() {
+        this.activeTerms += 1
+        if (this.activeTerms > 0) {
+            console.log("emitting active")
+            this.isactive.emit(true)
+        }
+    }
+
+}

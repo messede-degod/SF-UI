7b1f4e944b232e19e572de1bb325ad682d575090
user <you@example.com>
2023-05-13 13:18:25 +0530

    Improved Filebrowser

--- ui/src/app/pages/files-view/files-view.component.ts
+++ ui/src/app/pages/files-view/files-view.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { Config } from 'src/app/config/config';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { TerminalService } from 'src/app/services/terminal.service';
 
 @Component({
   selector: 'files-view',
@@ -10,31 +11,43 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class FilesViewComponent {
   FbIframeURL!: SafeUrl
-  @Input() ShowFrame: boolean = false
+
   @Input() InView: boolean = false
+  @Input() noOfTerminals: Number = 0
+
   FileBrowserActive: boolean = false
   FileBrowserDisconnected: boolean = false
-  FileBrowserNeedsTerminal: boolean = false
+  FileBrowserNeedsTerminal: boolean = true
   FirstStart: boolean = true
   CurrentTheme: string | null = ""
   DOMsanitizer!: DomSanitizer
-  @Input() noOfTerminals: Number = 0
+  terminalService: TerminalService
 
-  constructor(private sanitizer: DomSanitizer, private snackBar: MatSnackBar) {
+  constructor(private sanitizer: DomSanitizer, private snackBar: MatSnackBar, terminalService: TerminalService) {
     this.DOMsanitizer = sanitizer
     this.setFrameUrl()
+    this.terminalService = terminalService
+
+    terminalService.isactive.subscribe(active => {
+      if (active) {
+        if (this.FileBrowserNeedsTerminal) {
+          this.FileBrowserNeedsTerminal = false
+          this.FileBrowserActive = false
+          this.FirstStart = true
+        }
+      }
+      else {
+        this.FileBrowserNeedsTerminal = true
+      }
+    })
   }
 
   ngOnChanges() {
-    if (this.noOfTerminals==0){
-      this.FileBrowserNeedsTerminal = true
-    }else{
-      this.FileBrowserNeedsTerminal = false
-    }
-    if (this.ShowFrame && this.FirstStart) {
+    if (this.InView && this.FirstStart && !this.FileBrowserNeedsTerminal) {
       this.startFileBrowser()
       this.FirstStart = false
     }
+
     // Update Filebrowser if theme has changed
     if (this.InView) {
       if (this.CurrentTheme != localStorage.getItem("theme")) {

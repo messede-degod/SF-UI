51ea264b193de9e65d809f14d49d0934104c27a2
Messede <messede_degod@protonmail.com>
2024-04-06 11:29:03 +0000

    fix: docker build

--- other/docker/build_helper.go
+++ other/docker/build_helper.go
@@ -13,7 +13,7 @@ func main() {
 	build_hash := ""
 	dat, err := os.ReadFile("build_hash")
 	if err == nil {
-		build_hash = string(dat)[:7]
+		build_hash = string(dat)
 	}
 	build_cmd := fmt.Sprintf(`CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -tags prod -ldflags '-w' -ldflags '-X "main.buildTime=%s" -X "main.buildHash=%s"' -o sfui`, date, build_hash)
 	os.WriteFile("build.sh", []byte(build_cmd), 0644)
